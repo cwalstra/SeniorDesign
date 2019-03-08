75099b6c4d7dfaaa1930a2bdd0368d73b93d631d
cwalstra <cjwalstra@gmail.com>
2019-03-08 14:18:55 -0500

    Laid out command logic

---
+++ Command/commandLogic.py
@@ -0,0 +1,21 @@
+'''
+This file implements the command and control logic for the saving system.
+
+Written by Chris Walstra (@cwalstra)
+'''
+
+# Imports
+
+# Start a server to get information from the Pi processing the NoIR feed
+
+# try/while True:
+# Get information in from the thermal camera, the water level sensor, and the photoelectric eye
+
+# Store values in list to maintain memory
+
+# Evaluate for test conditions
+# Is there a person/people in the pool area/has there been in the past few samples (last 8 samples)
+# Does the thermal camera see someone recently (last 4 samples)?
+# Has the photoelectric eye been tripped in the past 4 samples?
+# Has the waterlevel sensor detected in a disturbance in the past 4 samples?
+
